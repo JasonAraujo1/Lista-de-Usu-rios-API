37c13c83e39d6fc3c91bc3cfb691bde0b08a32e9
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-02-12 22:14:37 -0300

    fix functions

--- src/pages/filtroPage/index.jsx
+++ src/pages/filtroPage/index.jsx
@@ -1,37 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchApiUsers } from '../../services/fetchApi'
+import { NavLink } from 'react-router'
 
 export default function Filtro() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        async function onLoad() {
+            const data = await fetchApiUsers()
+            setUsers(data)
+            console.log("DADOS FILTRO:", data)
+        }
+        onLoad()
+    }, [])
+
+
     return (
+
         <div className='flex gap-2  my-2 w-full sm:w-10 md:w-200 mx-auto'>
+
             <div className='border-2 p-3 gap-3 flex flex-col pr-15 '>
+
                 <h1 className='font-extrabold text-xl'>Filtra por:</h1>
-                <p>Publicações</p>
-                <p>Comentários</p>
-                <p>Albuns</p>
+                <p className='cursor-pointer'>Publicações</p>
+                <p className='cursor-pointer'>Comentários</p>
+                <p className='cursor-pointer'>Albuns</p>
             </div>
-            <div> 
-                <div>
-                    <div>
-                        <p>Fulano de tal</p>
-                        <p>Email@hotmail.com</p>
-                    </div>
-                    <div>
-                        <p>Endereço</p>
-                        <p>Pais, estado</p>
-                    </div>
+            <div>
+                {users.map((item) =>
+                    <>
+                        <div>
+                            <div>
+                                <p>{item.name}</p>
+                                <p>{item.email}</p>
+                            </div>
+                            <div>
+                                <p>Endereço: {item.address.street}, {item.address.suite}.</p>
+                                <p>{item.address.city}, zipcode: {item.address.zipcode}</p>
+                            </div>
+                        </div>
 
+                        <div className='border-2 mt-5 p-2 mb-16'>
+                            <h1 className='font-extrabold text-xl'>Resultado:</h1>
+                            <h2 className='font-extrabold'>Título</h2>
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ratione exercitationem repellend</h2>
 
-                </div>
-                <div className='border-2 mt-5 p-2'>
-                    <h1 className='font-extrabold text-xl'>Resultado:</h1>
-                    <h2 className='font-extrabold'>Título</h2>
-                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ratione exercitationem repellend</h2>
+                            <h2 className='font-extrabold'>Postagem:</h2>
+                            <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus tenetur similique corporis qui. Ullam eos iure tenetur dolorem incidunt, inventore aspernatur? Alias eveniet sequi voluptas quos nam eum quis odio!</p>
 
-                    <h2 className='font-extrabold'>Postagem:</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus tenetur similique corporis qui. Ullam eos iure tenetur dolorem incidunt, inventore aspernatur? Alias eveniet sequi voluptas quos nam eum quis odio!</p>
-                </div>
+                        </div>
+                    </>
+                )}
             </div>
+            <NavLink to='/home'>
+                <button className='outline-1 rounded-sm px-6 bg cursor-pointer mt-4'>Home</button>
+            </NavLink>
         </div>
+
     )
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { fetchApiUsers } from '../../services/fetchApi'
+import { NavLink } from 'react-router'
+import Filtro from '../filtroPage'
 
 export default function Home() {
   const [users, setUsers] = useState([])
@@ -19,10 +21,15 @@ export default function Home() {
     const user = users.filter(user => user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))//verifica se o parâmetro esta dentro do texto. ex: j esta dentro de jason? j está , então é true
     setUsers(user)
   }
+  function handleClickSelectOption(event) {//O evento gerado pelo <select> ao mudar de valor é automaticamente passado como argumento para a função.
+    const option = users.filter(({ address }) => address.city === event.target.value)
+    setUsers(option)
+  }
+
+
 
   return (
     <div>
-
 
       <main className='flex flex-col items-center'>
 
@@ -36,43 +43,52 @@ export default function Home() {
           </div>
 
           {/*SELECT OPTION*/}
+
           <div >
-            <select id="" className='outline-1 rounded-sm bg-gray-100 p-0.5 cursor-pointer'>
+            <select onChange={handleClickSelectOption} id="" className='outline-1 rounded-sm bg-gray-100 p-0.5 cursor-pointer'>
               <option value="" selected>cidades</option>
-              <option value="" ></option>
+              {users.map(({ address }) =>
+                <option value={address.city}> {address.city} </option>
+              )}
             </select>
           </div>
         </div>
 
         {/*TABELA*/}
         <div>
-         
-            <div>
-              <table className="border-collapse border border-gray-400 text-center w-full sm:w-10 md:w-200">
 
-                <tr className="bg-gray-200">
-                  <th className="border border-gray-400 px-4 py-2 text-2xl">Nome</th>
-                  <th className="border border-gray-400 px-4 py-2 text-2xl">E-mail</th>
-                  <th className="border border-gray-400 px-4 py-2 text-2xl">Cidade</th>
-                </tr>
+          <div>
+            <table className="border-collapse border border-gray-400 text-center w-full sm:w-10 md:w-200">
+
+              <tr className="bg-gray-200">
+                <th className="border border-gray-400 px-4 py-2 text-2xl">Nome</th>
+                <th className="border border-gray-400 px-4 py-2 text-2xl">E-mail</th>
+                <th className="border border-gray-400 px-4 py-2 text-2xl">Cidade</th>
+              </tr>
 
 
-                {users.map((element)=>(
-                  <tr>
-
+              {users.map((element) => (
+                <tr>
                   <td className="border border-gray-400 px-4 py-2">{element.name}</td>
                   <td className="border border-gray-400 px-4 py-2">{element.email}</td>
                   <td className="border border-gray-400 px-4 py-2">{element.address.city}</td>
-
                 </tr>
-                ))}
-              </table>
-            </div>
+              ))}
+            </table>
+           
+          </div>
 
           
 
+
+
         </div>
+        <NavLink to='/filtro'>
+          <button className='outline-1 rounded-sm px-6 bg cursor-pointer mt-4'>Filtros</button>
+        </NavLink>
+        
       </main>
+      
     </div>
   )
 }
